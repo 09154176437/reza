c1d29e7502fe4191999c866b8010d1bdb90d3b15
09154176437 <88371981+09154176437@users.noreply.github.com>
2021-09-04 12:28:04 +0430

    Add files via upload

---
+++ mabhas list.py
@@ -0,0 +1,10 @@
+nomarat = [20,19,17,18]
+print(nomarat)
+print(nomarat[2])
+print(nomarat[2:4])
+
+
+
+
+
+
---
+++ mabhas list2.py
@@ -0,0 +1,5 @@
+adad =[10,9,12,14,11,16,55,6,28,14]
+for ozv in adad:
+    if(ozv%2 == 0):
+        adad.remove(ozv)
+print(adad)
